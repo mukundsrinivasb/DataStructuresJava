fcb58c10e9829211184a15247d5cf82fdfb08d20
Mukund Balaji Srinivas <mukundbalaji.srinivas@anu.edu.au>
2021-06-28 00:43:14 +0530

    Made changes to remove method

--- tests/QueImplementationTest/QueTest.java
+++ tests/QueImplementationTest/QueTest.java
@@ -2,7 +2,6 @@ package QueImplementationTest;
 
 
 import QueueImplementation.Queue;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -10,15 +9,13 @@ import static org.junit.jupiter.api.Assertions.*;
 public class QueTest {
 
 
-
     @Test
     void addNoneTest() {
         Queue<Integer> queue = new Queue<>();
-        assertEquals(0,queue.size());
-        assertThrows(NullPointerException.class,()->{
-           queue.get();
-        });
+        assertEquals(0, queue.size());
+        assertThrows(NullPointerException.class, queue::get);
     }
+
     @Test
     void addOneTest() {
         Queue<Integer> queue = new Queue<>();
@@ -27,13 +24,43 @@ public class QueTest {
     }
 
     @Test
-    void addMoreThanOneTest(){
+    void addMoreThanOneTest() {
         Queue<Integer> queue = new Queue<>();
         queue.add(10);
         queue.add(20);
         queue.add(25);
-        assertEquals(3,queue.size());
+        assertEquals(3, queue.size());
 
     }
+
+    @Test
+    void removeNoneTest() {
+        Queue<Integer> queue = new Queue<>();
+        assertFalse(queue.remove());
+    }
+
+    @Test
+    void removeFromOneTest() {
+        Queue<Integer> queue = new Queue<>();
+        queue.add(1);
+        assertTrue(queue.remove());
+        assertEquals(0, queue.size());
+    }
+
+    @Test
+    void removeFromManyTest() {
+        Queue<String> queue = new Queue<>();
+        queue.add("Hello");
+        queue.add("World");
+        queue.add("India");
+        assertEquals(3, queue.size());
+        queue.remove();
+        assertEquals(2, queue.size());
+        assertEquals("World", queue.get());
+        queue.remove();
+        assertEquals(1, queue.size());
+        assertEquals("India", queue.get());
+    }
+
 
 }
