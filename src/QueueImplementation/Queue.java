01745360841b81b3a3484a6bad6b71c97cc6f2e3
Mukund Balaji Srinivas <mukundbalaji.srinivas@anu.edu.au>
2021-06-28 00:43:14 +0530

    Made changes to remove method

--- src/QueueImplementation/Queue.java
+++ src/QueueImplementation/Queue.java
@@ -14,11 +14,29 @@ public class Queue<T> implements fifo<T> {
             nextPtr = nextP;
         }
 
-        int sizeRecursive(QueueNode n){
-           if(n==end)
-               return 1;
-           return 1+sizeRecursive(n.nextPtr);
+        int sizeRecursive(QueueNode n) {
+            if (n == null)
+                return 0;
+            return 1 + sizeRecursive(n.nextPtr);
         }
+
+        void moveUp(QueueNode n) {
+            try {
+                QueueNode nNxt = new QueueNode(n.nextPtr.value, n.nextPtr.nextPtr);
+                if (n.nextPtr == end) {
+                    end = nNxt;
+                } else {
+                    if (n == start) {
+                        start = nNxt;
+                        moveUp(n.nextPtr);
+                    }
+                    moveUp(n.nextPtr);
+                }
+            } catch (NullPointerException e) {
+                System.out.println("Reached the end");
+            }
+        }
+
     }
 
 
@@ -28,29 +46,39 @@ public class Queue<T> implements fifo<T> {
         if (start == null) {
             start = addNode;
             end = start;
+        } else {
+            end.nextPtr = addNode;
+            end = end.nextPtr;
         }
-        end.nextPtr = addNode;
-        end = end.nextPtr;
 
     }
 
     @Override
     public boolean remove() {
-        return true;
+        if (start == null) {
+            return false;
+        } else {
+            if (start == end) {
+                start = null;
+                return true;
+            }
+            start.moveUp(start);
+            return true;
+        }
     }
 
     @Override
     public int size() {
-        try{
+        try {
             return start.sizeRecursive(start);
-        }catch (NullPointerException e){
+        } catch (NullPointerException e) {
             return 0;
         }
     }
 
     @Override
     public T get() {
-        if(start != null) {
+        if (start != null) {
             return start.value;
         }
         throw new NullPointerException("There is nothing to get");
