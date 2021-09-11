ddabf4f1c9f9878b25d2bec1e12e133bc0bed254
Mukund Balaji Srinivas <mukundbalaji.srinivas@anu.edu.au>
2021-09-11 20:55:08 +0530

    Understanding trees

--- src/Lab_04_DataStructure/UnderstandingTrees.java
+++ src/Lab_04_DataStructure/UnderstandingTrees.java
@@ -1,14 +1,16 @@
 package Lab_04_DataStructure;
 
+import java.util.Random;
+
 public class UnderstandingTrees {
     public static void main(String[] args) {
         BinarySearchTree FirstTree = new BinarySearchTree(5);
-        FirstTree = FirstTree.insert(10);
-        FirstTree = FirstTree.insert(4);
-        FirstTree = FirstTree.insert(3);
-        FirstTree = FirstTree.insert(12);
-        FirstTree = FirstTree.insert(9);
-        FirstTree = FirstTree.insert(2);
+        Random rand = new Random();
+        int temp = 0;
+        for (int i = 0; i < 5; i++){
+             temp = rand.nextInt(100);
+            System.out.print(temp+" ");
+            FirstTree = FirstTree.insert(temp);}
         System.out.print(FirstTree.display());
     }
 }
